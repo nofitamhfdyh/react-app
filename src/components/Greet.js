e354c6d05b94e456d1569c9e364b7369a9219055
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-20 09:48:47 +0700

    Using basic arrow function

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-function Greet(){
-        return <h1> Hallo Nupnup </h1>
-}
+// function Greet(){
+//         return <h1> Hallo Nupnup </h1>
+// }
+
+const Greet = () => <h1>Hallo Saudara Soudari</h1> // understanding the basic arrow functions
 
 export default Greet // supaya komponennnya bisa diakses dan dipanggil di folder lain
