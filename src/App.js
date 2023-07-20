0af965eaad3ca2466a34e57ae60f6cd0b2b6548e
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-20 20:08:53 +0700

    Using Event Handling (2)

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import Hello from './components/Hello';
 import Message from './components/Message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className="App">
+      <ClassClick>
+
+      </ClassClick>
       <FunctionClick>
-        
+
       </FunctionClick>
       {/* <Counter>
 
