45cd509f61f4a0bd6d95f750ff27934aa47121f7
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-20 10:51:36 +0700

    Tidak menggunakan JSX

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -2,13 +2,22 @@ import React from "react";
 
 
 const Hello = () => {
-        return(
-                <div>
-                        <h1>
-                                Hello Nopnopsss!!!
-                        </h1>
-                </div>
-        )
+        // return(
+        //         <div>
+        //                 <h1>
+        //                         Hello Nopnopsss!!!
+        //                 </h1>
+        //         </div>
+        // )
+
+        return React.createElement('div',
+                {id : 'Hello', className : 'dummyClass'}, // biasanya digunakan untuk menempatkan id dan class . jadi, jika tidak diperlukan sebuah id, maka bisa diisi null
+                React.createElement('h1', null, 'Holla! Nofita Mahfudiyah')) //(1)
+        /**
+         * dalam 1 function reacr.createElement(), hanya ada 3 parameter saja.
+         * jadi jika dalam 1 parameter memiliki anak, maka diparameter ke3, bisa dipanggil react.createElement()   
+         */
+
 }
 
 export default Hello
