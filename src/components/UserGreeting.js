8ecca576a48dc262e23198a12d54bb862bf5762f
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-21 15:08:24 +0700

    if else (3)

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -6,20 +6,25 @@ constructor(props){
         super(props)
 
         this.state = {
-                isLoggedIn: false
+                isLoggedIn: true
         }
 }
 
   render() {
+        return(
+                this.state.isLoggedIn ?
+                <div> Welcome Nofitaaaaaa</div> : 
+                <div> Welcome Guestttt</div>
+        )
 
-        let message //menyimpan elemen ke dalam message (element variable approach)
-        if (this.state.isLoggedIn){
-                message = <div> Welcome Nofitaaaaaa</div>
-        } else{
-                message = <div> Welcome Guest</div>
-        }
+        // let message //menyimpan elemen ke dalam message (element variable approach)
+        // if (this.state.isLoggedIn){
+        //         message = <div> Welcome Nofitaaaaaa</div>
+        // } else{
+        //         message = <div> Welcome Guest</div>
+        // }
 
-        return <div>{message}</div>
+        // return <div>{message}</div>
 
         // if (this.state.isLoggedIn) {
         //         return <div> Welcome Nupnup! </div>
