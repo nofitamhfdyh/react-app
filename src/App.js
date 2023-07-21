dfb9f2016ff4bd68bedc7a0011549c81548ce585
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-21 14:28:37 +0700

    Use .bind() in Binding Event Handlers

--- src/App.js
+++ src/App.js
@@ -7,14 +7,18 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
-      <ParentComponent>
+      {/* <ParentComponent>
         
-      </ParentComponent>
+      </ParentComponent> */}
+      <EventBind>
+        
+      </EventBind>
       {/* <ClassClick>
 
       </ClassClick>
