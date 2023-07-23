71fe03da3eccb65336fa846f86fb1122b30b0b90
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-23 14:20:09 +0700

    CSS multiple class (2)

--- src/components/StyleSheets.js
+++ src/components/StyleSheets.js
@@ -5,7 +5,7 @@ function StyleSheets(props) {
         let namaKelas = props.primary ? 'primary' : '' //membaca value dari primary props. jika value nya true, maka memanggil primary. settingan true / false di app.js (13)
   return (
     <div>
-        <h1 className={namaKelas}> Hello Nupnup</h1> {/** value dari variable namaKelas di assign ke className, sehingga isinya primary, lalu memanggil css .primary */}
+        <h1 className={`${namaKelas} font-xl`}> Hello Nupnup</h1> {/** value dari variable namaKelas di assign ke className, sehingga isinya primary, lalu memanggil css .primary */}
     </div>
   )
 }
