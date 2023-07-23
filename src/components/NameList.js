f43681a0cdbcae686df4254ef7c5f7038cf994bd
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-23 13:14:18 +0700

    List Rendering (2)

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,19 +2,10 @@ import React from "react";
 
 function NameList(){
         const names = ['Nofita', 'Mahfudiyah','Farhan','Rizky','Alkarim']
+        const nameList = names.map(names => <h2>{names}</h2>)
 
         return (
-                <div>
-                        {
-                                names.map(names => <h2>{names}</h2>)
-                        }
-
-                        {/* <h2>{names[0]}</h2>
-                        <h2>{names[1]}</h2>
-                        <h2>{names[2]}</h2>
-                        <h2>{names[3]}</h2>
-                        <h2>{names[4]}</h2> */}
-                </div>
+                <div>{nameList}</div>
         )
 }
 
