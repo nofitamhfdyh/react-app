bd643637bfadd2637ffa12283972fdc81c9004f1
nofitamahfudiyah <n.mahfudiyah@gmail.com>
2023-07-24 11:21:45 +0700

    form handling (2)

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,13 +5,27 @@ class Form extends Component {
                 super(props)
                 
                 this.state = {
-                        username: ''
+                        username: '',
+                        comments: '',
+                        topic:'nodejs'
                 }
         }
 
-        handleUsernameChange = (event) => {
+        handleUsernameChange = event => {
                 this.setState({
                         username: event.target.value
+                })
+        }
+
+        handleCommentsChange = event => {
+                this.setState({
+                        comments: event.target.value
+                })
+        }
+
+        handleTopicsChange = event => {
+                this.setState({
+                        topic: event.target.value
                 })
         }
   render() {
@@ -20,6 +34,18 @@ class Form extends Component {
         <div>
                 <label> Username </label>
                 <input type='text' value={this.state.username} onChange={this.handleUsernameChange}></input>
+        </div>
+        <div>
+                <label> Comments</label>
+                <textarea value={this.state.comments} onChange={this.handleCommentsChange}></textarea>
+        </div>
+        <div>
+                <label> Topic</label>
+                <select value={this.state.topic} onChange={this.handleTopicsChange}> 
+                        <option value="golang"> Golang</option>
+                        <option value="react"> React</option>
+                        <option value="nodejs"> NodeJS</option>
+                </select>
         </div>
       </form>
     )
